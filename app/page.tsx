1a414b6edb6de2f57dfb19ec5127e21db82bc288
jergra43 <jergra@yahoo.com>
2024-05-01 16:45:50 -0700

    border radius adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -358,7 +358,7 @@ const NewsCycle = () => {
           <Tagline>Stay informed, effortlessly.</Tagline>
         </HeaderContainer>
         <div className="flex flex-col items-center justify-between h-1/5">
-          <button onClick={() => newPage()} className="bg-orange-500 hover:bg-orange-700 text-white text-lg font-bold py-2 px-4 rounded">
+          <button onClick={() => newPage()} className="bg-orange-500 hover:bg-orange-700 text-white text-lg font-bold py-2 px-4 rounded-[5px]">
             Start
           </button>
           <SpeedSelect value={speed} onChange={handleSpeedChange}>
